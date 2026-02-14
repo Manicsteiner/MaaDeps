f8fadea53e1e051dccbd10bf6967cc156f7a0c46
Manicsteiner <Manicsteiner@outlook.com>
2026-02-14 22:13:48 +0800

    chore: try force 16k page without define

--- vcpkg-overlay/triplets/maa-arm64-android.cmake
+++ vcpkg-overlay/triplets/maa-arm64-android.cmake
@@ -7,7 +7,10 @@ set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS}
     -DANDROID_ABI=arm64-v8a
     -DANDROID_USE_LEGACY_TOOLCHAIN_FILE=OFF
-    -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-s")
+    -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-s -Wl,-z,max-page-size=0x4000 -Wl,-z,common-page-size=0x4000"
+    -DCMAKE_SHARED_LINKER_FLAGS_RELEASE="-Wl,-z,max-page-size=0x4000 -Wl,-z,common-page-size=0x4000"
+    -DCMAKE_EXE_LINKER_FLAGS="-Wl,-z,max-page-size=0x4000 -Wl,-z,common-page-size=0x4000"
+    -DANDROID_16KB_PAGES=ON)
 
 include(${CMAKE_CURRENT_LIST_DIR}/maa-linux-library-override.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/maa-android-library-override.cmake)
